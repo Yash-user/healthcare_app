75af875b46ac7666f0df5c55010d37b49ea1ef0a
Nikunj Sharma <nikunjsharma4218@gmail.com>
2025-09-05 22:20:45 +0530

    Added navbar

--- app/home.jsx
+++ app/home.jsx
@@ -1,11 +1,14 @@
 import { StyleSheet, Text, View } from 'react-native'
 
+
 export default function Home() {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
-  )
+    <>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Home Screen</Text>
+      </View>
+    </>
+  );
 }
 
 
